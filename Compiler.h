ad57f6359750cc32e1fc4f149f59ba68f5f1810f
Breno Rodrigues Marques da Silva <brenormsilva@gmail.com>
2016-06-10 20:29:11 -0300

    Update Compiler.h

--- Compiler.h
+++ Compiler.h
@@ -23,6 +23,83 @@ class Compiler{
         }
 
         int compile(){
+            string str;
+            queue<string> terms;
+        	while(getline(input, str)){
+        		istringstream iss(str);
+        		string buffer;
+        		while(iss >> buffer){
+        			cout << buffer << "\n";
+        			terms.push(buffer);
+        		}
+        		
+        		if(terms.front()[0] == '_'){ //label
+        			//put label in the table
+        		}
+        		else{ //opcode
+        			if(terms.front() == "MOV"){
+        				//binarycode << "1 em binario em 16bits?"
+        				//binarycode << (char)0 << (char)1 ?
+        				//update memPos
+        				terms.pop();
+        				if(terms.front()[0] > 64 && terms.front()[0] < 104){ //register
+        					if(terms.front()=="AX")
+        
+        					else if(terms.front()=="AL")
+        
+        					else if(terms.front()=="AH")
+        
+        					else if(terms.front()=="BX")
+        
+        					else if(terms.front()=="BL")
+        
+        					else if(terms.front()=="BH")
+        
+        					else if(terms.front()=="CX")
+        
+        					else if(terms.front()=="CH")
+        
+        					else if(terms.front()=="CL")
+        
+        				}else{ //memory
+        					//turn the address into binary?
+        				}
+        
+        				terms.pop();
+        				if(terms.front()[0] > 64 && terms.front()[0] < 104){ //register
+        					if(terms.front()=="AX")
+        
+        					else if(terms.front()=="AL")
+        
+        					else if(terms.front()=="AH")
+        
+        					else if(terms.front()=="BX")
+        
+        					else if(terms.front()=="BL")
+        
+        					else if(terms.front()=="BH")
+        
+        					else if(terms.front()=="CX")
+        
+        					else if(terms.front()=="CH")
+        
+        					else if(terms.front()=="CL")
+        
+        				}
+        				else if(terms.front().length == 5){ //immediate length=5?
+        					
+        				}
+        				else{//memory length!=5?
+        
+        				}
+        			}
+        			}else if(terms.front() == "ADD"){
+        
+        			}//repeat and adjust for each instruction
+        		}
+        
+        	}
+            
             return 1;
         }
 
