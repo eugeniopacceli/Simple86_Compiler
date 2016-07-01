adbe8d87670a7d9524a0c5712bd5795ea962ec7e
Breno Rodrigues Marques da Silva <brenormsilva@gmail.com>
2016-07-01 20:49:46 -0300

    Update op2.asm

--- tst/op2.asm
+++ tst/op2.asm
@@ -1,16 +1,16 @@
 _extern2: DW C
 MOV C, AX
 SUB AX, BX
-JS _a<b ;A<B
+JS _e2a<b ;A<B
 MOV C, BX
 SUB BX, CX
-JS _<c ;B<C
+JS _e2<c ;B<C
 WRITE CX
 RET
-_a<b: MOV AX, C
+_e2a<b: MOV AX, C
 SUB AX, CX
-JS _<c ;A<C
+JS _e2<c ;A<C
 WRITE CX
 RET 
-_<c: WRITE C
+_e2<c: WRITE C
 RET
